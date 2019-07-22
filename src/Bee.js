61e87bfbc520fb8b14f0ab0277ba80ec272416c0
Hack Reactor Student <student@hackreactor.com>
2019-07-22 12:46:08 -0500

    Clean slate 2

--- src/Bee.js
+++ src/Bee.js
@@ -2,7 +2,7 @@ var Bee = function() {
   Grub.call(this);
   this.age = 5;
   this.color = "yellow";
-  this.job = "Keep on growing";
+  this.job = "keep on growing";
 };
 
 Bee.prototype = Object.create(Grub.prototype);
